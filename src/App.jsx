8b410987973c1bd72b5e3cc8f1a3659ebca4b6f1
RoneyVaLo <rvaldelomarlopez@gmail.com>
2024-08-09 17:44:24 -0600

    implement section components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Economic from "./components/Economic"
+import Environmental from "./components/Environmental"
+import History from "./components/History"
+import Introduction from "./components/Introduction"
+import Navbar from "./components/Navbar"
+import Social from "./components/Social"
+
 function App() {
 
   return (
     <div>
-      ENCLAVE BANANERO
+      <Navbar />
+      <Introduction />
+      <History />
+      <Economic />
+      <Social />
+      <Environmental />
     </div>
   )
 }
